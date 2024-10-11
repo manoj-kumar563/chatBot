85effeb962465f1a744b76f092ef90a99c3ac7b0
manoj-kumar563 <55188215+manoj-kumar563@users.noreply.github.com>
2024-10-12 01:13:07 +0530

    chatBot initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {IStockData, IStockDetails, stockDataDetails} from './app.config'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'chatBot';
+export class AppComponent implements OnInit {
+  stockExchangeData:IStockData[] =[];
+  stockData:IStockDetails[]=[]; 
+  selectedStockExchange:string = '';
+  selectedStockName:string='';
+  stockExchangeSelected: boolean =false;
+  stockSelected: boolean =false;
+  selectedStockPrice: number = 0;
+  ngOnInit(){
+      this.stockExchangeData =stockDataDetails;
+    }
+
+  fetchStockDetails(stockData : IStockData):void{
+  
+    if(!this.stockExchangeSelected){
+      this.stockExchangeSelected  = true;
+      this.selectedStockExchange = stockData.stockExchange ? stockData.stockExchange :'';
+      this.fetchStockExchangeDetails(stockData);
+    }
+  
+  }
+
+  fetchStockExchangeDetails(stockData : IStockData):void{
+    setTimeout(()=>{ 
+      this.stockData = stockData.topStocks ? stockData.topStocks: []
+    },500)
+  }
+
+  fetchStockPrice(stock :IStockDetails) :void{
+    if(!this.stockSelected){
+      this.stockSelected  =true;
+      this.selectedStockName = stock.stockName;
+      setTimeout(()=>{ 
+        this.selectedStockPrice  = stock.price;
+      },500)
+    }
+  
+    
+  }
+  clearAll(){
+    this.stockExchangeData =[];
+    this.stockData=[]; 
+    this.selectedStockExchange= '';
+    this.selectedStockName='';
+    this.stockExchangeSelected =false;
+    this.stockSelected=false;
+    this.selectedStockPrice = 0;
+    this.stockExchangeData =stockDataDetails;
+  }
+  goBack(){
+    this.stockSelected  = false;
+    this.selectedStockName = '';
+  }
 }
